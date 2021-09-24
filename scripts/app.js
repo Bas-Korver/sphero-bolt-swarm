3a9652e354d13b5670aa714187aa0d1e46e15a7e
Bas Körver <bas_korver@outlook.com>
2021-09-24 10:16:44 +0200

    changes of last 2 weeks
    Rewrote character printing in two seperate functions,
    one to designate a color and letter to a bolt and one
    to print that information on the bolt's matrix
    
    started on opencv for bolt detection

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
-Vue.config.devtools = true
+Vue.config.devtools = true;
+ascii = Array.from(Array(26)).map((e, i) => i + 65);
+alphabet = ascii.map((x) => String.fromCharCode(x));
+colors = ['White']; // add more colors to support more than 26 bolts
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 var app = new Vue({
     el: "#app",
@@ -6,84 +14,139 @@ var app = new Vue({
         return {
             connectedBolts: [],
             selectedBolt: '',
+            possible_Combinations: colors.flatMap(d => alphabet.map(v => d + ' ' + v)),
+            rotateValue: 0,
         }
     },
     methods: {
-        async connectBolt() {
+        async vierkant() {
+            //let bolts = this.connectedBolts.slice(1, 3);
+            let bolts = this.connectedBolts
+            console.log("VIERKANT!")
 
+            bolts[1].roll(50, 0, []);
+            bolts[2].roll(50, 0, []);
+            await sleep(5000);
+
+            bolts[1].roll(50, 0, []);
+            bolts[2].roll(50, 0, []);
+            await sleep(5000);
+            bolts[1].roll(50, 0, []);
+            bolts[2].roll(50, 0, []);
+
+            console.log("go forward");
+            await sleep(5000);
+            console.log("Rotate")
+            bolts[1].roll(30, 270, []);
+            bolts[2].roll(30, 90, []);
+        },
+        async connectBolt() {
             bolt = new SpheroBolt();
             await bolt.connect();
             if (bolt.connected) {
                 console.log(bolt);
-                // console.log("Connected with BOLT");
-                this.connectedBolts.push({ botID: bolt });
+                this.connectedBolts.push(bolt);
+                this.boltAssignLetterColor(bolt);
+                this.boltPrintChar(bolt);
                 bolt.setMainLedColor(255, 255, 255);
                 bolt.setBackLedColor(255, 0, 0);
-                const listLength = this.connectedBolts.length;
 
-                switch (true) {
-                    case (listLength <= 26):
-                        let ascii = String.fromCharCode((listLength + 64));
-                        bolt.printChar(ascii, 255, 255, 255);
-                        this.connectedBolts[listLength - 1].botName = (ascii + ' Wit');
-                        break;
+                // console.log("Connected with BOLT");
+                // this.connectedBolts.push({ botID: bolt });
 
-                    case (listLength <= 52):
-                        ascii = String.fromCharCode(((listLength -= 26) + 64));
-                        bolt.printChar(ascii, 255, 0, 0);
-                        this.connectedBolts[listLength - 1].botName = (ascii + ' Rood');
-                        break;
-
-                    case (listLength <= 78):
-                        ascii = String.fromCharCode(((listLength -= 52) + 64));
-                        bolt.printChar(ascii, 0, 255, 0);
-                        this.connectedBolts[listLength - 1].botName = (ascii + ' Geel');
-                        break;
-
-                    case (listLength <= 104):
-                        ascii = String.fromCharCode(((listLength -= 78) + 64));
-                        bolt.printChar(ascii, 0, 0, 255);
-                        this.connectedBolts[listLength - 1].botName = (ascii + ' Blauw');
-                        break;
-                }
-                
                 bolt.on("onWillSleepAsync", () => {
-                    console.log('Waking up robot');
-                    bolt.wake();
+                    this.wakeAllBolts()
                 });
+
+                bolt.on("onCompassNotify", async (angle) => {
+                    bolt.setHeading(angle);
+                    for (let i = 0; i < this.connectedBolts.length; i++) {
+                        this.boltPrintChar(this.connectedBolts[i])
+                    }
+                });
+                // this.connectedBolts[0].botID.configureSensorStream()
                 // console.log(this.connectedBolts)
             }
         },
-
-        // Home made function
+        // Home made function for testing
         setColor(bolt, red, green, blue) {
             bolt.setMatrixColor(red, green, blue);
         },
         setColorToAll(red, green, blue) {
             for (let i = 0; i < this.connectedBolts.length; i++) {
                 // console.log("KLEUR AANGEPAST BOLT")
-                this.setColor(this.connectedBolts[i].botID, 0, 255, 0);
+                this.setColor(this.connectedBolts[i], 0, 255, 0);
             }
         },
         goForwardToAll(speed) {
             for (let i = 0; i < this.connectedBolts.length; i++) {
                 console.log("Go forward all");
-                this.connectedBolts[i].botID.roll(speed, 0, []);
+                this.connectedBolts[i].roll(speed, 0, []);
+            }
+        },
+        rotateAllBolts() {
+            for (let i = 0; i < this.connectedBolts.length; i++) {
+                console.log("Rotate all");
+                this.connectedBolts[i].setHeading(180);
             }
         },
         calibrateAll() {
             for (let i = 0; i < this.connectedBolts.length; i++) {
                 console.log("Calibrate all!");
-                this.connectedBolts[i].botID.calibrateToNorth();
+                this.connectedBolts[i].calibrateToNorth();
             }
         },
         collorOne() {
-            console.log(this.selectedBolt.botID)
-            this.setColor(this.selectedBolt.botID, 0, 0, 255);
+            console.log(this.selectedBolt)
+            this.setColor(this.selectedBolt, 0, 0, 255);
         },
         rotateBolt() {
             console.log(this.selectedBolt)
-            this.selectedBolt.botID.setHeading(45);
-        }
-    }
-})
+            this.selectedBolt.setHeading(this.rotateValue);
+        },
+        wakeAllBolts() {
+            console.log('Waking up robot');
+            for (let i = 0; i < this.connectedBolts.length; i++) {
+                this.connectedBolts[i].wake();
+            }
+        },
+        disconnectBolt() {
+            console.log(this.selectedBolt.name + ' ' + this.selectedBolt.color + ' bolt disconnected');
+            this.selectedBolt.disconnect();
+            const colorLetter = (this.selectedBolt.color + ' ' + this.selectedBolt.name);
+            this.possible_Combinations.push(colorLetter);
+            this.possible_Combinations.sort();
+            this.connectedBolts.splice((this.connectedBolts.indexOf(this.selectedBolt)), 1);
+        },
+        boltAssignLetterColor(bolt) {
+            let colorLetter = this.possible_Combinations.splice(0, 1);
+            colorLetter = colorLetter[0].split(' ');
+            bolt.name = colorLetter[1];
+            bolt.color = colorLetter[0];
+        },
+        boltPrintChar(bolt) {
+            switch (bolt.color) {
+                case ('Blue'):
+                    bolt.printChar(bolt.name, 0, 100, 255);
+                    break;
+
+                case ('Green'):
+                    bolt.printChar(bolt.name, 0, 255, 0);
+                    break;
+
+                case ('Red'):
+                    bolt.printChar(bolt.name, 255, 0, 0);
+                    break;
+
+                case ('White'):
+                    bolt.printChar(bolt.name, 255, 255, 255);
+                    break;
+            }
+        },
+    },
+});
+
+// Garbage collection
+delete ascii
+delete alphabet
+delete colors
