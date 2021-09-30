874da8af653112250965eff222bb43673e676f94
peppe0203 <1826689collura@zuyd.nl>
2021-09-30 13:56:32 +0200

    Define direction for the bolt to get to location

--- add_ons/openCV.py
+++ add_ons/openCV.py
@@ -3,6 +3,7 @@ import math
 from cv2 import cv2
 
 point_list = []
+center_location_frame = None
 
 
 def pointsInCircle(center=(0, 0), r=25, n=25):
@@ -15,6 +16,7 @@ def pointsInCircle(center=(0, 0), r=25, n=25):
         ) for x in range(0, n + 1)]
 
 
+# used for calculating the nearest point for the bolt
 def calculate_nearest_point(_x, _y):
     dis = 0
     closest = float("inf")
@@ -22,22 +24,47 @@ def calculate_nearest_point(_x, _y):
         if dis < math.sqrt((p[0] - _x) ** 2 + (p[1] - _y) ** 2):
             dis = math.sqrt((p[0] - _x) ** 2 + (p[1] - _y) ** 2)
             closest = point_list.index(p)
-    print(dis, " ", point_list[closest])
+    return point_list[closest]
+
+
+# used for controlling the bolt
+def control_bolt(_x1, _x2, _y1, _y2, _closest_point):
+    print(_closest_point)
+    if _y1 < int(_closest_point[1]) < _y2:
+        print("correct Y")
+    else:
+        # look for the position of the bolt compared to te closest point
+        # return values are the heading for the bolt
+        if _closest_point[1] > center_location_frame[1]:
+            if _y1 > _closest_point[1]:
+                return 180
+            elif _y1 < _closest_point[1]:
+                return 0
+        elif _closest_point[1] < center_location_frame[1]:
+            if _y1 < _closest_point[1]:
+                return 180
+            elif _y1 > _closest_point[1]:
+                return 0
 
 
 def checker(_x1, _y1, _x2, _y2):
     correct_position = False
+    # check if bolt is in the position
     for i in point_list:
         if _x1 < int(i[0]) < _x2 and _y1 < int(i[1]) < _y2:
-            print("Bolt in position")
+            # print("Bolt in position")
             correct_position = True
     if not correct_position:
-        calculate_nearest_point(_x1, _y1)
+        # look for the nearest point for the bolt
+        closest_point = calculate_nearest_point(_x1, _y1)
+        control_bolt(_x1, _x2, _y1, _y2, closest_point)
 
 
 # function for the webcam
 def openWebcam(_radius, _webcam=0, _tracking=True):
     global point_list
+    global center_location_frame
+
     cap = cv2.VideoCapture(_webcam)
 
     if not cap.isOpened():
@@ -49,6 +76,9 @@ def openWebcam(_radius, _webcam=0, _tracking=True):
         ret, frame = cap.read()
         (height, width) = frame.shape[:2]
         (circleCenterH, circleCenterW) = (height//2, width//2)
+
+        # get the center location of the frame as a global variable
+        center_location_frame = (circleCenterH, circleCenterW)
 
         # color is via BGR
         cv2.circle(frame, (circleCenterW, circleCenterH), _radius, (0, 0, 255), 2)
@@ -83,17 +113,12 @@ def openWebcam(_radius, _webcam=0, _tracking=True):
                     # check if the position of the bolt is correct
                     checker(x, y, (x + w), (y + h))
 
-        cv2.imshow("edit",hsv_frame)
+        cv2.imshow("edit", hsv_frame)
         cv2.imshow("frame", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             # clean all windows en stop capturing the camera feed
             cap.release()
             cv2.destroyAllWindows()
-
-        # ToDo: get rid of this when done with testing
-        if cv2.waitKey(1) & 0xFF == ord("w"):
-            # clean all windows en stop capturing the camera feed
-            print(point_list)
 
 
 # call function
