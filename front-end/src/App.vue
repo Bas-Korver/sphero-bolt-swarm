ac076489e479588e476ff797e406b1e0894f15d1
Cédric Cortenraede <c.cortenraede@outlook.com>
2021-10-16 20:57:19 +0200

    - Added screen if application is not connected to any BOLTs.

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,32 +1,27 @@
 <template>
   <div id="app" class="relative w-screen h-screen overflow-hidden font-sans">
-    <img
-      src=""
-      alt="Webcam feed"
-      class="w-full h-full bg-gray-200"
-      draggable="false"
-    />
-
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/4/45/LogoZUYD.png"
-      alt="Zuyd Hogeschool logo"
-      class="absolute w-20 right-10 top-10"
-      draggable="false"
-    />
-
-    <ActionBar />
-    <SettingsModal />
+    <div @click="bolts = ['For testing purposes']" v-if="!bolts.length">
+      <NotConnected />
+    </div>
+    <div v-else class="relative w-full h-full">
+      <Index />
+    </div>
   </div>
 </template>
 
 <script>
-import ActionBar from "./components/ActionBar.vue";
-import SettingsModal from "./components/modals/SettingsModal.vue";
+import Index from "./components/Index.vue";
+import NotConnected from "./components/NotConnected.vue";
 export default {
   name: "App",
+  data() {
+    return {
+      bolts: [],
+    };
+  },
   components: {
-    ActionBar,
-    SettingsModal,
+    Index,
+    NotConnected,
   },
 };
 </script>
---
+++ front-end/src/components/NotConnected.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Not connected with any BOLTs...</div>
+</template>
+
+<script>
+export default {
+  name: "NotConnected",
+};
+</script>
