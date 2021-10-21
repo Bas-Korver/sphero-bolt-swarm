1eb32ae7cf301667dd4194529197653c53c6cdf7
peppe0203 <1826689collura@zuyd.nl>
2021-10-21 13:30:46 +0200

    small edits

--- front-end/src/components/Index.vue
+++ front-end/src/components/Index.vue
@@ -3,7 +3,7 @@
     <img
       src="http://127.0.0.1:5000/feed"
       alt="Webcam feed"
-      class="fixed left-1/2 w-full bg-gray-200 object-cover"
+      class="fixed left-1/2 h-full bg-gray-200 object-cover"
       style="transform: translateX(-50%)"
       draggable="false"
     />
